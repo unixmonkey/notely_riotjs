3303fe744e4a07281b9949e34340e2a8b28e03a2
David Jones <unixmonkey1@gmail.com>
2015-11-16 19:18:51 -0500

    Add sign-in route.

--- public/src/app.js
+++ public/src/app.js
@@ -1,15 +1,26 @@
 (function () {
   'use strict';
 
+  var currentTag = null
+
   var routes = {
     default: function(id, action) {
-      riot.mount('notely', {
+      mount('notely', {
         stooges: [
           { name: 'Jim' },
           { name: 'Varney' }
         ]
       })
+    },
+
+    'sign-in': function(id, action) {
+      mount('sign-in');
     }
+  }
+
+  function mount(tag, options) {
+    currentTag && currentTag.unmount(true)
+    currentTag = riot.mount('notely', tag, options)[0]
   }
 
   var routeHandler = function(collection, id, action) {
