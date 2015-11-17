c89411cc30d1fd3766c537128e93527c8834f609
David Jones <unixmonkey1@gmail.com>
2015-11-16 19:05:26 -0500

    Create simple route handler.

--- public/src/app.js
+++ public/src/app.js
@@ -1,10 +1,29 @@
 (function () {
   'use strict';
-  riot.mount('notely', {
-    stooges: [
-      { name: 'Larry' },
-      { name: 'Curly' },
-      { name: 'Moe' }
-    ]
-  })
+
+  var routes = {
+    default: function(id, action) {
+      riot.mount('notely', {
+        stooges: [
+          { name: 'Jim' },
+          { name: 'Varney' }
+        ]
+      })
+    }
+  }
+
+  var routeHandler = function(collection, id, action) {
+    var routeStr = [collection, id, action].join('/')
+    console.log('routing ', routeStr)
+    var handler = routes[collection || 'default']
+    if (handler) {
+      handler(id, action)
+    } else {
+      console.log('no route found')
+    }
+  }
+
+  riot.route(routeHandler)
+  riot.route.start(routeHandler);
+
 }());
