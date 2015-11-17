d8d3de764448cefacedc5e36e2b82a5b5e66f2e5
David Jones <unixmonkey1@gmail.com>
2015-11-17 11:28:31 -0500

    Create Actions object for performing ajax requests

---
+++ public/src/actions.js
@@ -0,0 +1,40 @@
+(function () {
+  'use strict';
+
+  window.Actions = {
+
+    fetchNotes: function() {
+      return $.ajax({
+        url: CONSTANTS.API_BASE + 'notes',
+        method: 'GET',
+        headers: { 'Authorization': JSON.parse(localStorage.getItem('authToken')) }
+      })
+    },
+
+    authenticateUser: function(username, password) {
+      var credentials = {
+        user: {
+          username: username,
+          password: password
+        }
+      }
+      return $.ajax({
+        url: CONSTANTS.API_BASE + 'sessions',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(credentials)
+      })
+    },
+
+    createUser: function(userData) {
+      return $.ajax({
+        url: CONSTANTS.API_BASE + 'users',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ user: userData })
+      })
+    }
+
+  }
+
+})();
